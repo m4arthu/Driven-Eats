6cc7ece72e308964719fb6adabd9bd2363561df7
Luis Arthur <luisarthir259@gmail.com>
2023-03-31 14:48:31 -0300

    versão oficial  3.0 fim

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,17 @@ var total = []
 var checkp = ""
 var checkb = ""
 var checkd = ""
+
 function seletionPratos(id) {
   let button = document.getElementById("checkout-button")
   let prato1 = document.getElementById(id)
   let pratoid = prato1.id
   let numid = pratoid.match(/\d+/)[0]
-  let preço =(document.getElementById(`preço${numid}`).innerText)
+  let preço = document.getElementById(`preço${numid}`).innerText
   let check1 = document.getElementById(`itemcheck${numid}`)
 
   if (pratoId === "") {
+
     total.push(preço)
     prato += "Frango Yin Yang"
     pratoId = pratoid
@@ -26,13 +28,19 @@ function seletionPratos(id) {
   } else {
     let prato2 = document.getElementById(pratoId)
     let check2 = document.getElementById(checkp)
+
+    if (total.indexOf(document.getElementById(`preço${pratoId.match(/\d+/)[0]}`).innerText) !== -1) {
+      total.splice(total.indexOf(document.getElementById(`preço${pratoId.match(/\d+/)[0]}`).innerText),1)
+      total.push(preço)
+    }
+
     prato2.style.border = "none"
     prato1.style.border = "4px solid green"
     pratoId = pratoid
     checkp = check1.id
     check1.classList.remove("d-none")
     check2.classList.add("d-none")
-    
+
   }
 
   if (prato != "" && dessert != "" && bebida != "") {
@@ -47,26 +55,33 @@ function selectionDessert(id) {
   let dessert1 = document.getElementById(id)
   let dessertid = dessert1.id
   let numid = dessertid.match(/\d+/)[0]
-  let preço =(document.getElementById(`preço${numid}`).innerText)
+  let preço = document.getElementById(`preço${numid}`).innerText
   let check1 = document.getElementById(`itemcheck${numid}`)
 
   if (dessertId === "") {
+
     total.push(preço)
     dessert += "Pudim"
     dessertId = dessertid
-    checkb = check1.id
+    checkd = check1.id
     dessert1.style.border = "4px solid green"
     check1.classList.remove("d-none")
   } else {
     let dessert2 = document.getElementById(dessertId)
-    let check2 = document.getElementById(checkb)
+    let check2 = document.getElementById(checkd)
+
+    if (total.indexOf(document.getElementById(`preço${dessertId.match(/\d+/)[0]}`).innerText) !== -1) {
+      total.splice(total.indexOf(document.getElementById(`preço${dessertId.match(/\d+/)[0]}`).innerText), 1)
+      total.push(preço)
+    }
+
     dessert2.style.border = "none"
     dessert1.style.border = "4px solid green"
     dessertId = dessertid
-    checkb = check1.id
+    checkd = check1.id
     check1.classList.remove("d-none")
     check2.classList.add("d-none")
-    
+
   }
 
   if (prato != "" && dessert != "" && bebida != "") {
@@ -80,26 +95,33 @@ function selectionBebidas(id) {
   let bebida1 = document.getElementById(id)
   let bebidaid = bebida1.id
   let numid = bebidaid.match(/\d+/)[0]
-  let preço =(document.getElementById(`preço${numid}`).innerText)
+  let preço = document.getElementById(`preço${numid}`).innerText
   let check1 = document.getElementById(`itemcheck${numid}`)
 
   if (bebidaId === "") {
+
     total.push(preço)
     bebida += "Coca-cola"
     bebidaId = bebidaid
-    checkd = check1.id
+    checkb = check1.id
     bebida1.style.border = "4px solid green"
     check1.classList.remove("d-none")
   } else {
     let bebida2 = document.getElementById(bebidaId)
-    let check2 = document.getElementById(checkd)
+    let check2 = document.getElementById(checkb)
+
+    if (total.indexOf(document.getElementById(`preço${bebidaId.match(/\d+/)[0]}`).innerText) !== -1) {
+      total.splice(total.indexOf(document.getElementById(`preço${bebidaId.match(/\d+/)[0]}`).innerText), 1)
+      total.push(preço)
+    }
+
     bebida2.style.border = "none"
     bebida1.style.border = "4px solid green"
     bebidaId = bebidaid
-    checkd = check1.id
+    checkb = check1.id
     check1.classList.remove("d-none")
     check2.classList.add("d-none")
-    
+
   }
 
   if (prato != "" && dessert != "" && bebida != "") {
@@ -110,14 +132,32 @@ function selectionBebidas(id) {
 }
 
 function openModal() {
-    let  modal = document.querySelector("#confirm-menu")
+  let modal = document.querySelector("#confirm-menu")
+  let modalcont = document.querySelector(".modal-container")
+  let precop = document.querySelector(`#preço${pratoId.match(/\d+/)[0]}`).textContent
+  let precob = document.querySelector(`#preço${bebidaId.match(/\d+/)[0]}`).textContent
+  let precod = document.querySelector(`#preço${dessertId.match(/\d+/)[0]}`).textContent
+  let  TOTAL = 0 
 
-    modal.classList.remove("d-none")
+  for(let i = 0; i< total.length; i++) {
+    TOTAL += parseFloat(total[i])
+  }
+
+  modalcont.innerHTML += 
+  `<div style='display: flex; justify-content: space-between;'><p>${prato}<p><p>${precop}</p></div>
+   <div style='display: flex; justify-content: space-between;'><p>${bebida}<p><p>${precob}</p></div> 
+   <div style='display: flex; justify-content: space-between;'><p>${dessert}<p><p>${precod}</p></div>
+   <div style='display: flex; justify-content: space-between;'><p>Total<p><p>R$${TOTAL}</p></div>`
+
+
+  modal.classList.remove("d-none")
 }
 
 function closeModal() {
-  let  modal = document.querySelector("#confirm-menu")
+  let modal = document.querySelector("#confirm-menu")
+  let modalcont = document.querySelector(".modal-container")
 
+  modalcont.innerHTML = ""
   modal.classList.add("d-none")
 }
 
@@ -129,29 +169,32 @@ function send() {
     TOTAL += parseFloat(total[i])
   }
 
-  TOTAL += 2.70
 
-    var celular = "5599984592679";
-    
-    var texto = `Olá gostaria de fazer um pedido: \n -prato: ${prato}  \n -bebida: ${bebida} \n -sobremesa: ${dessert} \n -total: R$${TOTAL}0`
-    
-    texto = window.encodeURIComponent(texto);
-  
-    let urlApi = "https://web.whatsapp.com/send";
-    if(mobileCheck()){
-      urlApi = "https://api.whatsapp.com/send";
-    }
-    
-    window.open(urlApi + "?phone=" + celular + "&text=" + texto, "_blank");
+  var celular = "5599984592679";
 
+  var texto = `Olá gostaria de fazer um pedido: \n -prato: ${prato}  \n -bebida: ${bebida} \n -sobremesa: ${dessert} \n -total: R$${TOTAL}`
+
+  texto = window.encodeURIComponent(texto);
+
+  let urlApi = "https://web.whatsapp.com/send";
+  if (mobileCheck()) {
+    urlApi = "https://api.whatsapp.com/send";
   }
-  
+
+  window.open(urlApi + "?phone=" + celular + "&text=" + texto, "_blank");
+
+}
+
+// função  criada para testar se o usuário  estar no  mobile ou no pc com o  
   // função  criada para testar se o usuário  estar no  mobile ou no pc com o  
+// função  criada para testar se o usuário  estar no  mobile ou no pc com o  
+//fim  de  redirecionar o usuario para o wtts. Assím,  web caso  esteja  no  
   //fim  de  redirecionar o usuario para o wtts. Assím,  web caso  esteja  no  
-  //pc ou para a aplicação  caso esteja no  mobile
+//fim  de  redirecionar o usuario para o wtts. Assím,  web caso  esteja  no  
+//pc ou para a aplicação  caso esteja no  mobile
 
-  function mobileCheck(){
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-  }
+function mobileCheck() {
+  let check = false;
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+}
